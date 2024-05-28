9e6eb7b38d93eec5cdca1b8f90a493e37449c873
kitoumasaomi <kanfutrooper@gmail.com>
2024-05-28 16:29:03 +0900

    main.goを作成する

--- backend/main.go
+++ backend/main.go
@@ -8,6 +8,11 @@ func serverWs(w http.ResponseWriter, r *http.Request) {
     // WebSocketの接続処理をここに書く
 }
 
+func setupRoutes(){
+    
+}
+
 func main() {
+    setupRoutes()
     http.ListenAndServe(":9000", nil)
 };
