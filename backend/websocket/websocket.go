5e0ed265b1ebfb1ed2636f83d01ba9e9a81131c4
kitoumasaomi <kanfutrooper@gmail.com>
2024-05-28 11:19:18 +0900

    backend内にwebsocketフォルダとwebsocket.goを作成する

--- backend/websocket/websocket.go
+++ backend/websocket/websocket.go
@@ -1,17 +1,21 @@
 package customwebsocket
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gorilla/websocket"
 )
 
 var upgrader = websocket.Upgrader{
-	ReadBufferSize: 1024,
-	WriteBufferSize: 1024,
+    ReadBufferSize: 1024,
+    WriteBufferSize: 1024,
 }
 
-func Upgrade(w http.ResposeWriter, r *http.Request){
-	upgrader.CheckOrigin = func (r *http.Request)bool{return true}
-	upgrader.Upgrade(w, r, nil)
+func Upgrade(w http.ResponseWriter, r *http.Request) {
+    upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+    conn, err := upgrader.Upgrade(w, r, nil)
+    if err != nil {
+        log.Println("websocket connection error:", err)
+    }
 }
